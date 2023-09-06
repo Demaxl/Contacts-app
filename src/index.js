a91a9b418682c1c4f0a9259e830539cdbd46b9fe
demaxl <maxispeed42@gmail.com>
2023-09-06 20:44:01 +0100

    Added contact page to render in base template

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 
 // Creates browser router to enable client side routing
@@ -16,8 +17,18 @@ const router = createBrowserRouter([
         path: "/",
         element: <Root />,
         // The component rendered when we navigate to a page that doesnt match any route
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
+        // This is nested routing, the parent route(base layout) is rendered then the children are rendered
+        // depending on the url path
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />
+            }
+        ]
     },
+
+    
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
